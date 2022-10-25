db554c4e6573f58281c0ea0ed4703b6ac92723fa
L-ARAGAO <luizpaj@outlook.com>
2022-10-25 19:09:16 -0300

    adicao do script sql e alteracoes no projeto banco_awari

--- banco_awari/src/controller/account.controller.ts
+++ banco_awari/src/controller/account.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import AccountService from "../service/account.service";
+// import AccountService from "../service/account.service";
 
 
 export default class AccountController{
